b473b55bc59d02a0bcb1db669a1ec8f7a9826ad7
Ell Neal <elliot@emdentec.com>
2018-12-20 15:17:13 +0000

    Add total to itc report output

--- itcfinancereport/report.rb
+++ itcfinancereport/report.rb
@@ -1,3 +1,4 @@
+require 'bigdecimal'
 require 'csv'
 
 class Reporter
@@ -14,6 +15,7 @@ class Reporter
     def perform
         skip = 3
         n = 0
+        total = {}
         CSV.foreach(ARGV[0]) do |row|
             n += 1
             next if n <= skip
@@ -23,8 +25,20 @@ class Reporter
             owed = row[7].to_s
             exchange_rate = row[8].to_s
             proceeds = row[9].to_s
+            target_currency = row[10].to_s
 
-            puts "#{owed} #{currency} @ #{exchange_rate} for #{pluralize(units, "unit", "units")} ⇒ #{proceeds} GBP"
+            puts "#{owed} #{currency} @ #{exchange_rate} for #{pluralize(units, "unit", "units")} ⇒ #{proceeds} #{target_currency}"
+
+            if total[target_currency] != nil
+                total[target_currency] += BigDecimal.new(proceeds)
+            else
+                total[target_currency] = BigDecimal.new(proceeds)
+            end
+        end
+
+        puts
+        total.each_key do |key|
+            puts "#{total[key].to_s("F")} #{key}"
         end
     end
 end
